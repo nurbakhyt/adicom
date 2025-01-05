bb8a631fdd9e8c701fb3409dc430c620c6aff9d6
nurbakhyt <nurbakhyt@gmail.com>
2025-01-05 12:08:52 +0200

    add: ui-thing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,39 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ['@nuxtjs/strapi'],
-  strapi: {}
-})
+
+  modules: [
+    "@nuxtjs/strapi",
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/color-mode",
+    "@vueuse/nuxt",
+    "@nuxt/icon",
+    "@nuxt/fonts",
+  ],
+
+  strapi: {},
+
+  tailwindcss: {
+    exposeConfig: true,
+    editorSupport: true,
+  },
+
+  colorMode: {
+    classSuffix: "",
+  },
+
+  imports: {
+    imports: [
+      {
+        from: "tailwind-variants",
+        name: "tv",
+      },
+      {
+        from: "tailwind-variants",
+        name: "VariantProps",
+        type: true,
+      },
+    ],
+  },
+});

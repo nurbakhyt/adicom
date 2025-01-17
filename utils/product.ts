12dfd2b49a2f0c1fdf16778f59fc868cc83242e0
nurbakhyt <nurbakhyt@gmail.com>
2025-01-18 00:43:34 +0200

    add: migrating to cloudinary

--- utils/product.ts
+++ utils/product.ts
@@ -1,23 +1,10 @@
-export function getStrapiMedia(url: string | undefined): string {
-    const config = useRuntimeConfig()
-
-    if (url === undefined) {
-      return "/images/night-sky.avif";
-    }
-
-    if (url.startsWith("/")) {
-      return config.public.strapi.url ? `${config.public.strapi.url}${url}` : `http://127.0.0.1:1337${url}`;
-    }
-    return config.public.strapi.url ? `${config.strapiBaseUri}/${url}` : `http://127.0.0.1:1337/${url}`;;
+export function formatMoney(amount: number | undefined): string {
+  if (amount === undefined) {
+    return "--";
   }
 
-  export function formatMoney(amount: number | undefined): string {
-    if (amount === undefined) {
-      return "--";
-    }
-
-    return new Intl.NumberFormat('kz-KZ', {
-      style: 'currency',
-      currency: 'KZT',
-    }).format(amount);
-  }
+  return new Intl.NumberFormat("kz-KZ", {
+    style: "currency",
+    currency: "KZT",
+  }).format(amount);
+}
